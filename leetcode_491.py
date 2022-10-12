b60efcc73726c873e172645ce8590cdd1f19323d
Dreamer-Sea <chenzipeng10@126.com>
2022-10-13 01:07:50 +0800

    1. review leetcode_491 by Python3;

---
+++ leetcode_491.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.backtracking(nums, 0, [], res)
+        return res
+
+    def backtracking(self,
+                     nums: List[int], start: int, temp: List[int], res: List[List[int]]):
+        if start > len(nums):
+            return
+        if len(temp) > 1:
+            res.append([i for i in temp])
+        used = [False for _ in range(202)]
+        for i in range(start, len(nums)):
+            if used[nums[i]+100] or (len(temp) > 0 and nums[i] < temp[len(temp)-1]):
+                continue
+            used[nums[i]+100] = True
+            self.backtracking(nums, i+1, temp + [nums[i]], res)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.findSubsequences([4, 6, 7, 7]))
+
